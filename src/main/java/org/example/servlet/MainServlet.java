8bdb36c71d8f4de83865b27def8ab88d96dd1c16
Maksim <madmaxim22@gmail.com>
2023-03-26 15:54:51 +0300

    feat: implement DI with Spring xml config

--- src/main/java/org/example/servlet/MainServlet.java
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -1,63 +1,65 @@
 package org.example.servlet;
 
 import org.example.controller.PostController;
-import org.example.service.PostService;
-import org.example.repository.PostRepository;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-  private PostController controller;
-  private static final String GET = "GET";
-  private static final String POST = "POST";
-  private static final String DELETE = "DELETE";
-  private static final String PATH = "/api/posts";
-  private static final String PATH_POST_NUMBER = "/api/posts/\\d+";
-
-
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals(GET) && path.equals(PATH)) {
-        controller.all(resp);
-        return;
-      }
-      if (method.equals(POST) && path.equals(PATH)) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals(GET) && path.matches(PATH_POST_NUMBER)) {
-        // easy way
-        controller.getById(parseLong(path), resp);
-        return;
-      }
-      if (method.equals(DELETE) && path.matches(PATH_POST_NUMBER)) {
-        // easy way
-        controller.removeById(parseLong(path), resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    private PostController controller;
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private static final String PATH = "/api/posts";
+    private static final String PATH_POST_NUMBER = "/api/posts/\\d+";
+    
+    
+    @Override
+    public void init() {
+        final var factory = new DefaultListableBeanFactory();
+        final var reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions("beans.xml");
+        controller = (PostController) factory.getBean("postController");
+        
     }
-  }
-
-  private Long parseLong(String path) {
-    return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-  }
+    
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(GET) && path.equals(PATH)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(PATH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches(PATH_POST_NUMBER)) {
+                // easy way
+                controller.getById(parseLong(path), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches(PATH_POST_NUMBER)) {
+                // easy way
+                controller.removeById(parseLong(path), resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    private Long parseLong(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
 }
 
